c079d0150464103f8d514ef902fd1fdc308768db
Thomas Willingham <thomas@tshows.us>
2017-03-23 10:24:52 -0500

    functioning 'New Game' button on homepage

--- skorch/imports/ui/pages/Index.js
+++ skorch/imports/ui/pages/Index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
       <img src="/skorch-t72.png" width="40%"/>
       <p>A skorekeeping app for everything!</p>
-      <p><a className="btn btn-success" href="#" role="button">New Game</a>  <a className="btn btn-primary" href="#" role="button">Join Game</a></p>
+      <p><Link to="/games/new"><Button bsStyle="success">New Game</Button></Link>  <a className="btn btn-primary" href="#" role="button">Join Game</a></p>
     </Jumbotron>
   </div>
 );
