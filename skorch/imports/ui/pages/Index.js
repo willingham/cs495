c01bb48585291eab20511049d2f69a747b0e4ba8
Thomas Willingham <thomas@tshows.us>
2017-03-23 10:14:43 -0500

    updated front page

--- skorch/imports/ui/pages/Index.js
+++ skorch/imports/ui/pages/Index.js
@@ -4,10 +4,9 @@ import { Jumbotron } from 'react-bootstrap';
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
-      <h2>Base</h2>
-      <p>A starting point for Meteor applications.</p>
-      <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
-      <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.12.0</p>
+      <img src="/skorch-t72.png" width="40%"/>
+      <p>A skorekeeping app for everything!</p>
+      <p><a className="btn btn-success" href="#" role="button">New Game</a>  <a className="btn btn-primary" href="#" role="button">Join Game</a></p>
     </Jumbotron>
   </div>
 );
