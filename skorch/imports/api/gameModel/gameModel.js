4d3899762639bed43aa79165c1b3358b4749809d
Thomas Willingham <thomas@tshows.us>
2017-04-01 21:59:09 -0500

    moved to GameModelAPI branch

---
+++ skorch/imports/api/gameModel/gameModel.js
@@ -0,0 +1,73 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Factory } from 'meteor/dburles:factory';
+
+const GameModel = new Mongo.Collection('GameModel');
+export default GameModel;
+
+GamesModel.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false,
+});
+
+GameModel.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+});
+
+let player = new SimpleSchema({
+  player: {
+    type:String
+  },
+  score: {
+    type: Number
+  }
+});
+
+let gameOptions = new SimpleSchema({
+  players: {
+    type:[player],
+    optional: true
+  }
+});
+
+Games.schema = new SimpleSchema({
+  _id: { type: String, optional: true },
+  gameTitle: {
+    type: String,
+    label: 'The title of the game.',
+  },
+  gamePhrasePublic: {
+    type: String,
+    label: 'The public game phrase.',
+  },
+  gamePhrasePrivate: {
+    type: String,
+    label: 'The private game phrase.',
+  },
+  gameType: {
+    type: String,
+    label: 'The type of game.',
+  },
+  gameWinner: {
+    type: String,
+    label: 'The winner of the game.',
+  },
+  gameData: {
+    type: gameOptions,
+    optional: true
+  }
+});
+
+Games.attachSchema(Games.schema);
+
+Factory.define('game', Games, {
+  gameTitle: () => 'Game title',
+  gamePhrasePublic: () => 'Public game phrase',
+  gamePhrasePrivate: () => 'Private game phrase',
+  gameType: () => 'Type of game',
+  gameWinner: () => 'Winner of game',
+  gameData: () => 'Metadata of game',
+});
---
+++ skorch/imports/api/gameModel/server/publications.js
@@ -0,0 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import Games from '../games';
+
+Meteor.publish('games.list', () => Games.find());
+
+Meteor.publish('games.view', (_id) => {
+  check(_id, String);
+  return Games.find(_id);
+});
+
+Meteor.publish('games.phrase', (_phrase) => {
+  check(_phrase, String);
+  return Games.find({$or: [{gamePhrasePublic: _phrase}, {gamePhrasePrivate:_phrase}]});
+});
